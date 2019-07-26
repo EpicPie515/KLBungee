3d8138bc3b9a4db88120be89035c3891bf6e5db3
KJNine <joeykangaroo9@gmail.com>
2019-07-26 19:47:52 -0400

    Registered Playerinfo subcommand, Added Unknown Command checking.

--- src/main/java/lol/kangaroo/bungee/commands/AdminCommand.java
+++ src/main/java/lol/kangaroo/bungee/commands/AdminCommand.java
@@ -3,6 +3,7 @@ package lol.kangaroo.bungee.commands;
 import java.util.Arrays;
 
 import lol.kangaroo.bungee.commands.admin.CmdlistSub;
+import lol.kangaroo.bungee.commands.admin.PlayerinfoSub;
 import lol.kangaroo.bungee.commands.admin.TestvoteSub;
 import lol.kangaroo.bungee.player.PlayerManager;
 import lol.kangaroo.bungee.util.Message;
@@ -19,6 +20,7 @@ public class AdminCommand extends CommandExecutor {
 		super(pm, proxy, "admin", Rank.SRMOD.getPerm(), "adm", "control", "con");
 		
 		registerSubcommand(new TestvoteSub(pm, proxy, this));
+		registerSubcommand(new PlayerinfoSub(pm, proxy, this));
 		
 		
 		registerSubcommand(new CmdlistSub(pm, proxy, this, subCommands.values()));
@@ -27,35 +29,43 @@ public class AdminCommand extends CommandExecutor {
 	@Override
 	public void execute(ProxiedPlayer sender, BasePlayer bp, String label, String[] args) {
 		if(args.length == 0) {
-			Message.sendMessage(bp, MSG.PREFIX_ERROR, MSG.COMMAND_ADMIN_USAGE);
+			Message.sendMessage(bp, MSG.PREFIX_ADMIN, MSG.COMMAND_ADMIN_USAGE);
 			return;
 		}
+		boolean cmdFound = false;
 		String[] subArgs = Arrays.copyOfRange(args, 1, args.length);
 		for(String k : subCommands.keySet()) {
 			if(k.equalsIgnoreCase(args[0])) {
 				Subcommand s = subCommands.get(k);
+				cmdFound = true;
 				if(sender.hasPermission(s.getPermission()))
 					s.execute(sender, bp, args[0], subArgs);
 				else
-					Message.sendMessage(bp, MSG.NO_PERM);
+					Message.sendMessage(bp, MSG.PREFIX_ERROR, MSG.NO_PERM);
 				break;
 			}
 		}
+		if(!cmdFound)
+			Message.sendMessage(bp, MSG.PREFIX_ERROR, MSG.UNKNOWN_COMMAND);
 	}
 
 	@Override
 	public void executeConsole(String label, String[] args) {
 		if(args.length == 0) {
-			Message.sendConsole(MSG.PREFIX_ERROR, MSG.COMMAND_ADMIN_USAGE);
+			Message.sendConsole(MSG.PREFIX_ADMIN, MSG.COMMAND_ADMIN_USAGE);
 			return;
 		}
+		boolean cmdFound = false;
 		String[] subArgs = Arrays.copyOfRange(args, 1, args.length);
 		for(String k : subCommands.keySet()) {
 			if(k.equalsIgnoreCase(args[0])) {
+				cmdFound = true;
 				subCommands.get(k).executeConsole(args[0], subArgs);
 				break;
 			}
 		}
+		if(!cmdFound)
+			Message.sendConsole(MSG.PREFIX_ERROR, MSG.UNKNOWN_COMMAND);
 	}
 	
 }
