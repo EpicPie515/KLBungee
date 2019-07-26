c353cadf66f0c784c8a1a2b3d89bee14951c5124
Cameron Feezel <camfeezel@gmail.com>
2019-07-26 19:02:02 -0400

    Added Runnable for checking and removing expired ranks and permissions.

---
+++ src/main/java/lol/kangaroo/bungee/permissions/RankPermissionExpiry.java
@@ -0,0 +1,28 @@
+package lol.kangaroo.bungee.permissions;
+
+import java.util.concurrent.TimeUnit;
+
+import lol.kangaroo.bungee.KLBungeePlugin;
+import lol.kangaroo.common.player.CachedPlayer;
+
+public class RankPermissionExpiry implements Runnable {
+	
+	private KLBungeePlugin plugin;
+	
+	public RankPermissionExpiry(KLBungeePlugin pl) {
+		plugin = pl;
+	}
+	
+	public void startLoopTask() {
+		plugin.getProxy().getScheduler().schedule(plugin, this, 30, 60, TimeUnit.SECONDS);
+	}
+
+	@Override
+	public void run() {
+		for(CachedPlayer cp : plugin.getPlayerManager().getPlayerCacheManager().getPlayerCache()) {
+			plugin.getPlayerManager().demoteIfRankExpired(cp);
+			plugin.getPlayerManager().removeExpiredPermissions(cp);
+		}
+	}
+	
+}
