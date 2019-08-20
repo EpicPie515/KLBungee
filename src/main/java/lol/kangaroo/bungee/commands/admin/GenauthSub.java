fe10f006cfec04e4222e52683446345567a7e772
KJNine <joeykangaroo9@gmail.com>
2019-08-20 19:36:29 -0400

    Removed self-generation and added authenticator requirement

--- src/main/java/lol/kangaroo/bungee/commands/admin/GenauthSub.java
+++ src/main/java/lol/kangaroo/bungee/commands/admin/GenauthSub.java
@@ -1,7 +1,7 @@
 package lol.kangaroo.bungee.commands.admin;
 
-import com.vexsoftware.votifier.bungee.events.VotifierEvent;
-import com.vexsoftware.votifier.model.Vote;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import lol.kangaroo.bungee.commands.AdminCommand;
 import lol.kangaroo.bungee.commands.Subcommand;
@@ -19,51 +19,65 @@ import net.md_5.bungee.api.connection.ProxiedPlayer;
 
 public class GenauthSub extends Subcommand {
 
+	private static final Pattern argPattern = Pattern.compile("^-([a-z][a-z]*)=(.+)$", Pattern.CASE_INSENSITIVE);
+	
 	public GenauthSub(PlayerManager pm, ProxyServer proxy, AdminCommand parent) {
 		super(pm, proxy, parent, "genauth", Rank.ADMIN_DEV.getPerm(), "getauth", "createauth");
 	}
 
 	@Override
 	public void execute(ProxiedPlayer sender, BasePlayer bp, String label, String[] args) {
-		// TODO REQUIRE AUTH TO GENERATE (and i guess remove self-generating) and ADMIN COMMAND
 		ThreadManager.async(() -> {
-			if(args.length == 0) {
-				if(Auth.hasSecret(bp)) {
-					Message.sendMessage(bp, MSG.PREFIX_ERROR, MSG.COMMAND_ADMIN_GENAUTH_ALREADY);
-					return;
-				}
-				String secret = Auth.generateAndAssignSecret(bp);
-				Message.sendMessage(bp, MSG.PREFIX_ADMIN, MSG.COMMAND_ADMIN_GENAUTH, secret);
-			} else {
-				CachedPlayer target = pm.getCachedPlayer(pm.getFromCurrentExact(args[0]));
-				if(target == null) {
-					Message.sendMessage(bp, MSG.PREFIX_ERROR, MSG.PLAYER_NOTFOUND);
-					return;
-				}
-				ProxiedPlayer pp = proxy.getPlayer(target.getUniqueId());
-				if(pp == null) {
-					Message.sendMessage(bp, MSG.PREFIX_ERROR, MSG.PLAYER_NOTFOUND);
-					return;
-				}
-				if(Auth.hasSecret(target)) {
-					Message.sendMessage(bp, MSG.PREFIX_ERROR, MSG.COMMAND_ADMIN_GENAUTH_OTHER_ALREADY);
-					return;
-				}
-				String secret = Auth.generateAndAssignSecret(target);
-				Message.sendMessage(target, MSG.PREFIX_ADMIN, MSG.COMMAND_ADMIN_GENAUTH, secret);
-				Message.sendMessage(bp, MSG.COMMAND_ADMIN_GENAUTH_OTHER, pm.getRankManager().getPrefix(target) + target.getVariable(PlayerVariable.USERNAME));
+			if(args.length <= 1 || argPattern.matcher(args[0]).matches()) {
+				Message.sendMessage(bp, MSG.PREFIX_ERROR, MSG.COMMAND_ADMIN_GENAUTH_USAGE);
+				return;
 			}
+			CachedPlayer target = pm.getCachedPlayer(pm.getFromCurrentExact(args[0]));
+			if(target == null) {
+				Message.sendMessage(bp, MSG.PREFIX_ERROR, MSG.PLAYER_NOTFOUND);
+				return;
+			}
+			Matcher match = argPattern.matcher(args[1]);
+			if(!match.matches() || !(match.group(1).toLowerCase().charAt(0) == 'a')) {
+				Message.sendMessage(bp, MSG.PREFIX_ERROR, MSG.COMMAND_ADMIN_GENAUTH_USAGE);
+				return;
+			}
+			String authStr = match.group(2);
+			int authCode = 0;
+			try {
+				authCode = Integer.parseInt(authStr);
+			} catch(NumberFormatException e) {
+				Message.sendMessage(bp, MSG.PREFIX_ERROR, MSG.ADMIN_AUTH_FAIL);
+				return;
+			}
+			if(!Auth.validate(bp, authCode, 20000)) {
+				Message.sendMessage(bp, MSG.PREFIX_ERROR, MSG.ADMIN_AUTH_FAIL);
+				return;
+			}
+			Rank r = pm.getRankManager().getRank(target, false);
+			String targetName = pm.getRankManager().getPrefix(target) + target.getVariable(PlayerVariable.USERNAME);
+			if(!r.isStaff()) {
+				Message.sendMessage(bp, MSG.PREFIX_ERROR, MSG.COMMAND_ADMIN_GENAUTH_NOTSTAFF, targetName, r.getColor() + r.name());
+				return;
+			}
+			ProxiedPlayer pp = proxy.getPlayer(target.getUniqueId());
+			if(pp == null) {
+				Message.sendMessage(bp, MSG.PREFIX_ERROR, MSG.PLAYER_NOTFOUND);
+				return;
+			}
+			if(Auth.hasSecret(target)) {
+				Message.sendMessage(bp, MSG.PREFIX_ERROR, MSG.COMMAND_ADMIN_GENAUTH_OTHER_ALREADY);
+				return;
+			}
+			String secret = Auth.generateAndAssignSecret(target);
+			Message.sendMessage(target, MSG.PREFIX_ADMIN, MSG.COMMAND_ADMIN_GENAUTH, secret);
+			Message.sendMessage(bp, MSG.COMMAND_ADMIN_GENAUTH_OTHER, pm.getRankManager().getPrefix(target) + target.getVariable(PlayerVariable.USERNAME));
 		});
 	}
 
 	@Override
 	public void executeConsole(String label, String[] args) {
-		if(args.length == 0) {
-			Message.sendConsole(MSG.PREFIX_ERROR, MSG.COMMAND_ADMIN_TESTVOTE_USAGE);
-			return;
-		}
-		proxy.getPluginManager().callEvent(new VotifierEvent(new Vote("Manual Vote", args[0], "", "")));
-		Message.sendConsole(MSG.PREFIX_ADMIN, MSG.COMMAND_ADMIN_TESTVOTE, args[0]);
+		Message.sendConsole(MSG.PREFIX_ERROR, MSG.MUST_BE_PLAYER);
 	}
 	
 	
